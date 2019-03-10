3847b6b34717157a8e7e476fa904ef4dcbe84e76
Dan Naeem <dan.naeem95@gmail.com>
2019-03-10 18:36:21 -0400

    created SinglyLinkedList.py, implemented Node class

---
+++ SinglyLinkedList.py
@@ -0,0 +1,8 @@
+################################################################
+class Node(object):
+    def __init__(seld, valu=None, next=None):
+        """initializes value and next to None by default"""
+
+        self.value = value
+        self.next = next
+################################################################
