36d8b4e5b5e80c3923daafd4b5a38c1795cbdf76
Dan Naeem <dan.naeem95@gmail.com>
2019-03-10 21:48:33 -0400

    fixed typos

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -1,7 +1,7 @@
 ################################################################
 class Node(object):
-    def __init__(seld, valu=None, next=None):
-        """initializes value and next to None by default"""
+    def __init__(self, value=None, next=None):
+        """Initializes value and next to None by default."""
 
         self.value = value
         self.next = next
